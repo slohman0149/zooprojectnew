0685aefd4d3cde095fb29b77e1b3f06de95e7a7a
Sydney <slohman0149@myduneland.org>
2020-09-15 09:00:22 -0500

    Initial sharing of project

---
+++ Axotol.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Lion here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Axotol
+{
+    // instance variables - replace the example below with your own
+    private int age;
+
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void eats()
+    {
+        System.out.println("nom nom nom");
+    }
+}
---
+++ ZooProgram.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class ZooProgram here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ZooProgram
+{
+    public static void main (String [] args) {
+        Axotol Ali = new Axotol();
+        Ali.eats();
+    } // end of main
+}  
